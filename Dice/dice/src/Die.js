4cd254d9eda4658b9e990a9bf4cfe325dd66c177
Wanyue Xiao <wxiao5@illinois.edu>
2021-03-25 10:48:53 -0500

    add studcture

---
+++ Dice/dice/src/Die.js
@@ -0,0 +1,12 @@
+import react,{Component} from 'react';
+import './Die.css';
+
+
+class Die extends Component{
+    render(){
+        return(
+        <h1>Die</h1>)
+    }
+}
+
+export default Die;
---
+++ Dice/dice/src/RollDice.js
@@ -0,0 +1,13 @@
+import react,{Component} from 'react';
+import Die from './Die';
+import './RollDice.css';
+
+
+class RollDice extends Component{
+    render(){
+        return(
+        <h1>RollDice</h1>)
+    }
+}
+
+export default RollDice;
